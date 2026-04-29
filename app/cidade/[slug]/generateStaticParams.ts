8956a35724ac2c144fbbcd611ef5e89a9e8fa1da
parceiro ton autorizado <tonparceirosmart.suporte@gmail.com>
2026-04-29 14:03:19 -0300

    Update generateStaticParams.ts

--- app/cidade/[slug]/generateStaticParams.ts
+++ app/cidade/[slug]/generateStaticParams.ts
@@ -1,5 +1,16 @@
-const bairros: Record<string, string[]> = {
+type Cidade = "curitiba" | "londrina" | "maringa";
+
+const bairros: Record<Cidade, string[]> = {
   curitiba: [],
   londrina: [],
   maringa: []
+};
+
+for (const cidade in bairros) {
+  for (const bairro of bairros[cidade as Cidade]) {
+    paths.push({
+      slug: cidade,
+      bairro: bairro,
+    });
+  }
 }
