fcb26d7554d955f4a31ec3ff9096fa60acf5389b
parceiro ton autorizado <tonparceirosmart.suporte@gmail.com>
2026-04-29 14:26:45 -0300

    Update generateStaticParams.ts

--- app/cidade/[slug]/generateStaticParams.ts
+++ app/cidade/[slug]/generateStaticParams.ts
@@ -1,8 +1,16 @@
-Object.entries(bairros).forEach(([cidade, listaBairros]) => {
-  listaBairros.forEach((bairro) => {
-    paths.push({
-      slug: cidade,
-      bairro,
-    });
-  });
-});
+import { bairros } from "@/bairros"
+
+export async function generateStaticParams() {
+  const paths: { slug: string; bairro: string }[] = []
+
+  Object.entries(bairros).forEach(([cidade, listaBairros]) => {
+    listaBairros.forEach((bairro) => {
+      paths.push({
+        slug: cidade,
+        bairro,
+      })
+    })
+  })
+
+  return paths
+}
